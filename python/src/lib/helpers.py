a851d9a34eb4346655d8c53c81d2dd5d8cf7d323
Seb Wiechers <sebastiaanwiechers@icloud.com>
2025-03-12 15:31:18 +0100

    feature: proper data structures

--- python/src/lib/helpers.py
+++ python/src/lib/helpers.py
@@ -1,11 +1,28 @@
-def idx(*x): return x
+unit   = ()
+idx    = lambda *args: args
+wrap   = lambda s   : f"({s})"
+plus   = lambda a, b: a + b
+swap   = lambda a, b: b, a
+unless = lambda x   : 
+         lambda *_  : x
+left   = lambda f: lambda x, *xs: f(x)
+right  = lambda f: lambda x, *xs: f(*xs)
+apply  = lambda f, g    : 
+         lambda x, *xs  :
+         f(x), g(*xs)
+foldl  = lambda bind:
+         lambda f, g : 
+         lambda x, *xs  : bind (
+         f(x), 
+         g(*xs)
+    )
 
-wrap = lambda s: f"[{s}]"
-plus = lambda a, b: a + b
-make = lambda x: lambda *_: x
-apply = lambda f, g: lambda x, *xs: f(x), g(*xs)
-foldl = lambda f, g, Z: lambda x, *xs: Z(f(x), g(*xs))
-
+foldr   = lambda bind:
+          lambda M, unit: 
+          lambda a, b: 
+              bind ( a, unit ( 
+              bind ( M, b ) 
+              ))
 
 def snake(*args):
     ob = iter(args)
@@ -23,59 +40,39 @@ def M(F, G):
 
 
 map = lambda f: M(
-      make(None),
-      dist(f, map)
+      unless (None),
+      apply  (f, map)
     )
 
-bind = lambda f: M(
-       make(""),
-       foldl(f, bind(f), plus)
-    )
+monoid = lambda unit, functor, bind: M (
+         unless (unit),
+         foldl  (bind) ( 
+            functor, 
+            monoid (unit, functor, bind)
+            )
+        )
 
+// yields x1(:: x2)
+push    = foldr (plus) (":: ", wrap)
 
-def sum(*args):
-    return M(make(0), bind(idx))
+// yields callback(x2, idx), x1
+unshift = foldr (swap) (idx, callback)
+
+concat  = monoid ("", idx, plus)
+sum     = monoid (0, idx, plus)
+
+// yields (x1)(x2)...
+wrapMap = monoid ("", wrap, plus)
+
+// yields (:: (x1)(:: (x2) ...
+wrapList= monoid ("", wrap, push)
+
+wrapThen = lambda callback: M(
+           right (callback), 
+           wrap, // has to be foldr ...
+           )
+
 
 def hLog(*args):
-    return lambda x: print(bind(wrap)(*args) + x)
-x
-
-wrap = lambda s: f"[{s}]"
-plus = lambda a, b: a + b
-make = lambda x: lambda *_: x
-apply = lambda f, g: lambda x, *xs: f(x), g(*xs)
-foldl = lambda f, g, Z: lambda x, *xs: Z(f(x), g(*xs))
-
-
-def snake(*args):
-    ob = iter(args)
-
-    try: return next(ob), ob
-    except: return ((),())
-
-def M(F, G):
-    def Fn(*args):
-        hd, tl = snake(*args)
-
-        if hd == (): return F(hd, tl)
-        return G(hd, tl)
-    return Fn
-
-
-map = lambda f: M(
-      make(None),
-      dist(f, map)
-    )
-
-bind = lambda f: M(
-       make(""),
-       foldl(f, bind(f), plus)
-    )
-
-
-def sum(*args):
-    return M(make(0), bind(idx))
-
-def hLog(*args):
-    return lambda x: print(bind(wrap)(*args) + x)
-
+    return lambda x: print(wrapMap(*args) + x)
+he definition given by
